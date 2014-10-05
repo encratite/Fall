455bedc3ecf46b1bf4e8b6b06d583fa072cbf195
void <git.email.address@googlemail.com>
2014-10-06 01:40:47 +0200

    Added optimisation

--- Build.sh
+++ Build.sh
@@ -6,14 +6,13 @@ outputDirectory=Output
 mkdir -p $objectDirectory
 mkdir -p $outputDirectory
 
-
 objects=
 for sourceFile in $sourceFiles
 do
 	outputFile=$(basename $sourceFile | cut -d. -f1).o
 	object=$objectDirectory/$outputFile
 	echo Building $sourceFile
-	g++ -c $sourceFile -o $object -fPIC -std=c++11 -I.
+	g++ -c $sourceFile -o $object -fPIC -std=c++11 -O3 -I.
 	if [[ $? != 0 ]]
 	then
 		exit
